32f61cf0b8765ab08e323785211a8e822bb6847c
ExpCoder <camero2711@gmail.com>
2023-12-31 12:15:21 -0800

    Version 2.0

--- gameloader.js
+++ gameloader.js
@@ -81,9 +81,125 @@ var gamesText = `{
         },
         {
           "name":"Brick Breaker",
-          "img":"img/brickbr.png",
+          "img":"img/brbr.png",
           "path":"Brick_Breaker-master/index.html"
+        },
+        {
+          "name":"Minesweeper",
+          "img":"img/minesw.webp",
+          "path":"minesweep/index.html"
+        },
+        {
+          "name":"Google Dino",
+          "img":"img/dino.png",
+          "path":"googdino/index.html"
+        },
+        {
+          "name":"Mario Portal",
+          "img":"img/ma0.jpg",
+          "path":"mario/index.html"
+        },
+        {
+          "name":"Super Mario 64",
+          "img":"img/m64.png",
+          "path":"sm64/index.html"
+        },
+        {
+          "name":"Tetris",
+          "img":"img/tetris.png",
+          "path":"tetris/index.html"
+        },
+        {
+          "name":"Pokemon Fire",
+          "img":"img/pkmr.webp",
+          "path":"Pokemon%20Fire%20Red/index.html"
+        },
+        {
+          "name":"Pokemon Green",
+          "img":"img/pkmg.jpg",
+          "path":"Pokemon%20Leaf%20Green/index.html"
+        },
+        {
+          "name":"2048",
+          "img":"img/2048img.png",
+          "path":"2048/index.html"
+        },
+        {
+          "name":"Sonic Advance",
+          "img":"img/sonica.jpg",
+          "path":"emu/GBA/sonic-advance/index.html"
+        },
+        {
+          "name":"Super Mario World",
+          "img":"img/smw.jpg",
+          "path":"emu/SNES/smw/index.html"
+        },
+        {
+          "name":"Super Mario Kart",
+          "img":"img/smk.png",
+          "path":"emu/SNES/smk/index.html"
+        },
+        {
+          "name":"Legend Of Zelda",
+          "img":"img/loz.webp",
+          "path":"emu/SNES/loz/index.html"
+        },
+        {
+          "name":"Bitlife",
+          "img":"img/bitlife.png",
+          "path":"Bitlife/index.html"
+        },
+        {
+          "name":"Retrobowl",
+          "img":"img/bowl.png",
+          "path":"retrobowl/index.html"
+        },
+        {
+          "name":"NES Emulator",
+          "img":"img/nes.png",
+          "path":"nes/index.html"
+        },
+        {
+          "name":"RUN 3",
+          "img":"img/run3.png",
+          "path":"RUN3/index.html"
+        },
+        {
+          "name":"Cookie Clicker",
+          "img":"img/cookieclicker.png",
+          "path":"cookieclicker/index.html"
+        },
+        {
+          "name":"MotoX3M",
+          "img":"img/motox3m.png",
+          "path":"motox3m/index.html"
+        },
+        {
+          "name":"Fluid Simulation",
+          "img":"img/fluidsim.png",
+          "path":"Fluid-Simulation/index.html"
+        },
+        {
+          "name":"Wordle",
+          "img":"img/wordlelogo.webp",
+          "path":"Wordle/index.html"
+        },
+        {
+          "name":"Subway Surfers",
+          "img":"img/subwaysurfers.webp",
+          "path":"subway/index.html"
+        },
+        {
+          "name":"Sandboxels",
+          "img":"img/sandboxels.jfif",
+          "path":"sandboxels/index.html"
+        },
+        {
+          "name":"Flappy Bird",
+          "img":"img/flappybird.jpg",
+          "path":"flappy/index.html"
         }
+
     ]
 }`;
 var gameObject = JSON.parse(gamesText);
